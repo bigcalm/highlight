ab44713b243a4ffe75ab67e8c00e926128d55cc7
Terence Eden <git@shkspr.mobi>
2025-04-22 18:18:32 +0100

    Removed tests. Tidy HTML5 icon

--- autoload.php
+++ autoload.php
@@ -1,28 +1,28 @@
 <?php
 // autoload.php
 
-spl_autoload_register(function ($class) {
-    // Project-specific namespace prefix
-    $prefix = 'Tempest\\Highlight\\';
+spl_autoload_register( function ( $class ) {
+    //  Project-specific namespace prefix
+    $prefix = "Tempest\\Highlight\\";
 
-    // Base directory for the namespace prefix
+    //  Base directory for the namespace prefix
     $base_dir = __DIR__ . "/src/";
 
-    // Does the class use the namespace prefix?
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
+    //  Does the class use the namespace prefix?
+    $len = strlen( $prefix );
+    if ( strncmp( $prefix, $class, $len ) !== 0) {
         // No, move to the next registered autoloader
         return;
     }
 
-    // Get the relative class name
-    $relative_class = substr($class, $len);
+    //  Get the relative class name
+    $relative_class = substr( $class, $len );
 
-    // Replace namespace separators with directory separators, append with .php
-    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    //  Replace namespace separators with directory separators, append with .php
+    $file = $base_dir . str_replace( "\\", "/", $relative_class ) . ".php";
 
     // If the file exists, require it
-    if (file_exists($file)) {
+    if ( file_exists( $file ) ) {
         require $file;
     }
 });
--- tests/targets/test.md
+++
@@ -1,5 +0,0 @@
-```php
-public function __construct(
-    private(set) Author $author,
-) {}
-```
